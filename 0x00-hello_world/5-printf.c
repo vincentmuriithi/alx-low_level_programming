94dd462fca96dd87d5495c915db8ad427c2f9513
vincentmuriithi <vincentmuriithi06@gmail.com>
2023-07-16 06:13:45 -0700

    5-printf.c code

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main -entry for program
+ *
+ * Return: 0 upon success
+ */
+int main(void)
+{
+	printf("%S", "with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
