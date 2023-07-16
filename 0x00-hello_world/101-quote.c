5636889a82cc1c5245eb7c5e637c112ffc247936
vincentmuriithi <vincentmuriithi06@gmail.com>
2023-07-16 08:02:26 -0700

    101-quote.c  code

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,12 @@
-#include <unistd.h>
-
+#include<unistd.h>
+/**
+ * main -entry for program
+ *
+ * Return: will return 1 upon execution
+ */
 int main(void)
 {
-    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-    return 1;
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
 
