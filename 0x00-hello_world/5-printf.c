d5606ad10b084c42f2d7b968562e02998510949d
vincentmuriithi <vincentmuriithi06@gmail.com>
2023-07-16 06:17:43 -0700

    5-printf.c code

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-	printf("%S", "with proper grammar, but the outcome is a piece of art,\n");
+	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
