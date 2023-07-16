b22c62de807aa6479aa04f0f8253330569800800
vincentmuriithi <vincentmuriithi06@gmail.com>
2023-07-16 05:42:18 -0700

    4-puts.c code

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/*
+ * main - entry function for program
+ * prints the given string using puts function
+*/
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
