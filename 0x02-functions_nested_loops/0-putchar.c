d42b0e37f99e09b1f4da7a9a23beb56f3613ae73
vincentmuriithi <vincentmuriithi06@gmail.com>
2023-07-19 03:59:33 -0700

    putchar.o code update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,11 @@
-#include<unistd.h>
+#include "main.h"
+#include<stdio.h>
 #include<string.h>
-#ifndef MAIN_H
-#define MAIN_H
-int _putchar(char c);
-#endif /* MAIN_H*/
 /**
  * main - entry of program
  *
  * Return: resturns 0 upon success
  */
-int  _putchar(char c)
-{
-	return write(1,&c,1);
-}
 int main(void)
 {
     char tr[] = "_putchar";
