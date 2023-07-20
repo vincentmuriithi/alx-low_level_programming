926f07d00fd79f18b14a3277833245dd6cdfaaa6
vincentmuriithi <vincentmuriithi06@gmail.com>
2023-07-20 13:20:15 -0700

    positive_or_negative()  code

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - entry of program,tests the positive_or_negative() function
+ * Return: 0 u[on success
+ */
+int main(void)
+{
+	int k;
+	positive_or_negative(k);
+	return (0);	
+}
