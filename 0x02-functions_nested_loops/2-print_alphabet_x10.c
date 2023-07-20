31d8014b9d9a3b4d0e19242c295d26f88590f6f8
vincentmuriithi <vincentmuriithi06@gmail.com>
2023-07-20 03:26:38 -0700

    2-print_alphabet_x10.c code update

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include "main.h"
+#include "alpha10.h"
 /**
  * main - entry of program
  * Return: 0 upon success
---
+++ 0x02-functions_nested_loops/alpha10.h
@@ -0,0 +1,4 @@
+#ifndef ALPHA10_H
+#define ALPHA10_H
+void print_alphabetx10(void);
+#endif 
