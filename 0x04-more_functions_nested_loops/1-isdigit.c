9553b3ec31c5421f1e1edf7ca0dfe91e14c3d923
vincentmuriithi <vincentmuriithi06@gmail.com>
2023-07-22 01:13:48 -0700

    digit checker code

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isdigit - checks for digit
+ * Returns: 1 if a digit is found and returns 0 if it is not found
+ */
+int  _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
