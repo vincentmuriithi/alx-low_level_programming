ad2c34a67d96c0350b586c3e00281f25c7b7afca
vincentmuriithi <vincentmuriithi06@gmail.com>
2023-07-23 09:28:25 -0700

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * print_triangle - draws triangle as per given input
+ * @n: the size of triangle
+ * Return: returns void
+ */
+void print_triangle(int n)
+{
+	int k, i;
+
+	if (n == 0)
+	{
+		_putchar(' ');
+		return;
+	}
+
+	for (k = 0; k < n; k++)
+	{
+		for ( i = 0; i < n - k - 1; i++)
+		{
+			_putchar(' ');
+
+		}
+		for (i = 0; i < k + 1; i++)
+		{
+			_putchar('#');
+		}
+
+		_putchar('\n');
+	}
+}
