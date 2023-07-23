73e311c1a0bb125ff6d77ca22a84020b6d5ba13b
vincentmuriithi <vincentmuriithi06@gmail.com>
2023-07-23 07:35:49 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,36 +1,35 @@
 #include<stdio.h>
 /**
- * main - entry of program
- * Return: 0 upon success
- */
+* main - entry of program
+* Return: 0 upon success
+*/
 int main(void)
 {
-	int k;
+int k;
 
-	for (k = 1; k <= 100; k++)
-	{
-		if (k % 5 == 0 && k % 3 == 0)
-		{
-			printf("FizzBuzz ");
-			continue;
+for (k = 1; k <= 100; k++)
+{
+if (k % 5 == 0 && k % 3 == 0)
+{
+printf("FizzBuzz ");
 
-		}
-		else if (k == 5 || k % 5 == 0)
-		{
-			printf("Buzz ");
-			continue;
+}
+else if (k == 5 || k % 5 == 0)
+{
+printf("Buzz ");
 
-		}
-		else if (k % 3 == 0)
-		{
-			printf("Fizz ");
-			continue;
+}
+else if (k % 3 == 0)
+{
+printf("Fizz ");
 
-		}
+}
+else
+{
+printf("%d ", k);
+}
+}
 
-		printf("%d ", k);
-	}
-
-	printf("\n");
-	return (0);
+printf("\n");
+return (0);
 }
