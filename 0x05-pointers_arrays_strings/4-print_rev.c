ad1b95ecb7f2283d62becfb633fc4e41a58aaca5
vincentmuriithi <vincentmuriithi06@gmail.com>
2023-07-24 13:35:59 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,11 +7,12 @@
 void print_rev(char *s)
 {
 	int length = 0;
+	int i;
 
 	while (s[length] != '\0')
 		length++;
 
-	for (int i = length - 1; i >= 0; i--)
+	for (i = length - 1; i >= 0; i--)
 		_putchar(s[i]);
 
 	_putchar('\n');
