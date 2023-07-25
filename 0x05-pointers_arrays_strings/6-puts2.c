5934e7b8374820a8a152d2591d842fe41060f08b
vincentmuriithi <vincentmuriithi06@gmail.com>
2023-07-25 02:45:49 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * puts2 - prints every other character of tghe string
+ * @str: input
+ * Return: returns void
+ */
+void puts2(char *str)
+{
+	int length = 0;
+
+	while (*str != '\0')
+	{
+		_putchar(str[length]);
+		length += 2;
+	}
+
+	_putchar('\n');
+}
