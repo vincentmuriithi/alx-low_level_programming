a8b96750a5b6a1911d3ea482e206c952c96e4c45
vincentmuriithi <vincentmuriithi06@gmail.com>
2023-07-25 03:44:48 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+ * puts_half - printas the 2nd half of string if its even
+ * @str: input pointer
+ * Return: returns void
+ */
+void puts_half(char *str)
+{
+	int k, i, half, n;
+
+	k = 0;
+
+	while (*str != '\0')
+	{
+		k++;
+	}
+
+	half = k / 2;
+	i = k - 1;
+	n = (k - 1) / 2;
+
+	while (str  > 0)
+	{
+		if (half % 2 == 0)
+		{
+			_putchar(*(str + i));
+		}
+		else
+		{
+			if (i < n)
+			{
+				_putchar(*(str + i));
+			}
+		}
+
+		i++;
+
+	}
+
+	_putchar('\n');
+}
