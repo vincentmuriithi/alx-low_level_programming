ea8d9520ded3de15ded9f7f50f6d82e79cba4856
vincentmuriithi <vincentmuriithi06@gmail.com>
2023-07-25 05:34:08 -0700

    101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define PASSWORD_LENGTH 10
+
+char generateRandomChar() {
+	return (rand() % 94 + 33);
+}
+
+int main(void) {
+	srand(time(NULL));
+
+	char password[PASSWORD_LENGTH + 1];
+	int i;
+
+	for (i = 0; i < PASSWORD_LENGTH; i++) {
+		password[i] = generateRandomChar();
+	}
+
+	password[PASSWORD_LENGTH] = '\0';
+
+	printf("%s\n", password);
+
+	return (0);
+}
