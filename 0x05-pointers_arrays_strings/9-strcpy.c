c75866a076110b80cf58112e72e82d3b34407eb1
vincentmuriithi <vincentmuriithi06@gmail.com>
2023-07-25 04:35:28 -0700

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* _strcpy - copies 2nd string to first pointer parameter
+* @dest: null pointer
+* @src: pointer whose content is to be copied
+* Return: returns pointer dest
+*/
+char *_strcpy(char *dest, char *src)
+{
+*dest = *src;
+return (*dest);
+}
