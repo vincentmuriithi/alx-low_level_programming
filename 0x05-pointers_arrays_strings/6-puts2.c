8f4007f4f3512a07bce668d607dc9aa452a97bf1
vincentmuriithi <vincentmuriithi06@gmail.com>
2023-07-25 02:52:40 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,12 +6,12 @@
  */
 void puts2(char *str)
 {
-	int length = 0;
+/*	int length = 0;*/
 
 	while (*str != '\0')
 	{
-		_putchar(str[length]);
-		length += 2;
+		_putchar(*str);
+		str += 2;
 	}
 
 	_putchar('\n');
