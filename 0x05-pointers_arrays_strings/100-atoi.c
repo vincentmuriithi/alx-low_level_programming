e1a2da6cea02a30eb529eeba903a2f34241b6554
vincentmuriithi <vincentmuriithi06@gmail.com>
2023-07-25 05:24:23 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -24,6 +24,10 @@ int _atoi(char *s)
 
 	while (s[i] >= '0' && s[i] <= '9')
 	{
+		if (result > INT_MAX / 10 || (result == INT_MAX / 10 && s[i] - '0' > INT_MAX % 10))
+		{
+			return (sign == 1) ? INT_MAX : INT_MIN;
+		}
 		result = result * 10 + (s[i] - '0');
 		i++;
 	}
