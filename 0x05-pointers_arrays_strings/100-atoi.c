23715f8912b34b60df491d8c21a4b2d6edd9b95d
vincentmuriithi <vincentmuriithi06@gmail.com>
2023-07-25 05:26:12 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -15,19 +15,18 @@ int _atoi(char *s)
 	while (s[i] == ' ' || (s[i] >= 9 && s[i] <= 13))
 		i++;
 
-	while (s[i] == '-' || s[i] == '+')
+	if (s[i] == '-')
 	{
-		if (s[i] == '-')
-			sign *= -1;
+		sign = -1;
+		i++;
+	}
+	else if (s[i] == '+')
+	{
 		i++;
 	}
 
 	while (s[i] >= '0' && s[i] <= '9')
 	{
-		if (result > INT_MAX / 10 || (result == INT_MAX / 10 && s[i] - '0' > INT_MAX % 10))
-		{
-			return (sign == 1) ? INT_MAX : INT_MIN;
-		}
 		result = result * 10 + (s[i] - '0');
 		i++;
 	}
