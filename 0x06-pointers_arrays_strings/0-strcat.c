c1885beca47e0faf0eb8685855882a083510c890
vincentmuriithi <vincentmuriithi06@gmail.com>
2023-07-28 06:03:37 -0700

    strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,11 +12,13 @@ char *_strcat(char *dest, char *src)
 	int j;
 
 	i = 0;
+
 	while (dest[i] != '\0')
 	{
 		i++;
 	}
 	j = 0;
+
 	while (src[j] != '\0')
 	{
 		dest[i] = src[j];
@@ -25,5 +27,6 @@ char *_strcat(char *dest, char *src)
 	}
 
 	dest[i] = '\0';
+
 	return (dest);
 }
