adfccc4fce5e71ad885f990fb58e6994cf19ba11
vincentmuriithi <vincentmuriithi06@gmail.com>
2023-07-28 05:53:59 -0700

    strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strcat - concatenates strings
+ * @dest: input pointer
+ * @src: input pointer
+ * Return: pointer
+ *
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *new = dest;
+
+	while (*dest)
+	{
+		new++;
+	}
+
+	while (*src)
+	{
+		*new = *src;
+		new++;
+		src++;
+	}
+
+	*new = '\0';
+
+	return (dest);
+}
