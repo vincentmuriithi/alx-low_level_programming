3ee11d682c398164d694cda4a10aa9f149be3058
vincentmuriithi <vincentmuriithi06@gmail.com>
2023-07-28 06:44:38 -0700

    1-strcnt.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _strcnt - concatenates two strings with a limitation of n bytes
+ * @dest: input poointer(destination)
+ * @src: input pointer source
+ * @n: input integer(number of bytes)
+ * Return: returns the new concatenated string as a pointer
+ */
+char *_strcnt(char *dest, char *src, int n)
+{
+	int i, j, k;
+
+	while (dest[i])
+	{
+		i++;
+	}
+
+	k = 0;
+
+	while (k < n && src[k])
+	{
+		dest[i] = src[k];
+		i++;
+		k++;
+	}
+
+	dest[i] =  '\0';
+
+	return (dest);
+}
