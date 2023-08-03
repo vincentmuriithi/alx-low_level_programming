dba04f87f9061ae8472220af87e68ac6896e2759
vincentmuriithi <vincentmuriithi06@gmail.com>
2023-08-03 13:20:47 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -6,28 +6,18 @@
 */
 void _print_rev_recursion(char *s)
 {
-int i, k;
 
-i = 0;
-k = 0;
-
-while (*s != '\0')
-{
-i++;
-}
-
-if (s[i] < 0)
+if (*s == '\0')
 {
 _putchar('\0');
-_putchar('\n');
 return;
 }
 
-k = i;
 
-_putchar(s[k]);
-k--;
-_print_rev_recursion(s + k);
+
+_print_rev_recursion(s + 1);
+
+_putchar(*s);
 
 
 }
