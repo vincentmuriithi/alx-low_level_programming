40f96969d420b8148a8e1988ebe6329fa77d1415
vincentmuriithi <vincentmuriithi06@gmail.com>
2023-08-04 15:51:27 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 /**
- * main - main entry of program
- * @argc: number of arguments
- * @argv: the argument vector
- * Return: returns 0  upon success
- */
+* main - main entry of program
+* @argc: number of arguments
+* @argv: the argument vector
+* Return: returns 0  upon success
+*/
 int main(int argc, char **argv)
 {
-	(void)argv;
-	printf("%d\n", argc);
+(void)argv;
+printf("%d\n", argc);
 
-	return (0);
+return (0);
 }
