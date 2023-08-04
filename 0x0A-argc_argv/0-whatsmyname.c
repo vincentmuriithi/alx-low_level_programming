bd515054a7681c9461301a9011985b9277333f77
vincentmuriithi <vincentmuriithi06@gmail.com>
2023-08-04 14:56:45 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - main entry of program
+* @argc: command line arguments
+* @argv: value of commandline arguments
+* Return: returns 0 upon success
+*/
+int main(int argc, int *argv)
+{
+printf("%s", argv[0]);
+
+return (0);
+}
