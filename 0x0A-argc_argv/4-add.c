fbeafae4928fdc505467b359c5988997bb4aaba1
vincentmuriithi <vincentmuriithi06@gmail.com>
2023-08-08 17:03:46 -0700

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - entry of main program
+ * @argc: input
+ * @argv: input
+ * Return: returns 0 upon success
+ */
+int main(int argc, char *argv[])
+{
+	int sum;
+
+	sum = 0;
+
+	if (argc == 0)
+		printf("%d\n", 0);
+
+	for (int i = 1; i < argc; i++) {
+        for (int j = 0; argv[i][j] != '\0'; j++) {
+            if (!isdigit(argv[i][j]) && argv[i][j] != '-') {
+                printf("Error\n");
+                return 1;
+            }
+        }
+        sum += atoi(argv[i]);
+    }
+
+    printf("%d\n", sum);
+    return 0;
+}
+
+}
