9bfbaedda0d7e8930c61c42b38a89624ef5b0f46
vincentmuriithi <vincentmuriithi06@gmail.com>
2023-08-10 05:14:50 -0700

    1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+* _strdup - returns a pointer to a newly allocated space
+* Return: returns pointer upon success
+*/
+char *_stdup(char *str)
+{
+char *duplicate;
+unsigned int length = 0, i;
+
+if (str == NULL)
+return (NULL);
+
+while (str[length])
+length++;
+
+duplicate = malloc(sizeof(char) * (length + 1));
+
+if (duplicate == NULL)
+return (NULL);
+
+for (i = 0; i <= length; i++)
+duplicate[i] = str[i];
+
+return (duplicate);
+}
