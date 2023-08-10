a68ccc396b912d30212901a35351f8d3961e40ea
vincentmuriithi <vincentmuriithi06@gmail.com>
2023-08-10 11:33:58 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -8,6 +8,7 @@
 int **alloc_grid(int width, int height)
 {
 int **grid, i, j;
+
 if (width <= 0 || height <= 0)
 return (NULL);
 
@@ -15,20 +16,17 @@ grid = (int **)malloc(height * sizeof(int *));
 if (grid == NULL)
 return (NULL);
 
-// Allocate memory for columns in each row and initialize to 0
 for (i = 0; i < height; i++)
 {
 grid[i] = (int *)malloc(width * sizeof(int));
 if (grid[i] == NULL)
 {
-// If memory allocation fails, free previously allocated memory
 for (j = 0; j < i; j++)
 free(grid[j]);
 free(grid);
 return (NULL);
 }
 
-// Initialize each element to 0
 for (j = 0; j < width; j++)
 grid[i][j] = 0;
 }
