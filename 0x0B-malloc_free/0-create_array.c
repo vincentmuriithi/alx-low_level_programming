d53c1044803b20594713586d07129d2171b3075e
vincentmuriithi <vincentmuriithi06@gmail.com>
2023-08-10 04:41:37 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,28 +1,28 @@
 #include "main.h"
 /**
- * create_array - entry of program
- * @size: input
- * @c: input
- * Return: returns a non null pointer upon success
- */
+* create_array - entry of program
+* @size: input
+* @c: input
+* Return: returns a non null pointer upon success
+*/
 char *create_array(unsigned int size, char c)
 {
-	char *ptr;
-	unsigned int i;
+char *ptr;
+unsigned int i;
 
-	if (size == 0)
-	{
-		ptr = NULL;
-		return (ptr);
-	}
+if (size == 0)
+{
+ptr = NULL;
+return (ptr);
+}
 
-	ptr = (char*)malloc(size * sizeof(char));
+ptr = (char*)malloc(size * sizeof(char));
 
-	for (i = 0; i < size; i++)
-	{
-		ptr[i] = c;
-	}
+for (i = 0; i < size; i++)
+{
+ptr[i] = c;
+}
 
-	return (ptr);
+return (ptr);
 
 }
