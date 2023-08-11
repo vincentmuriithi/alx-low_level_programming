9c9a84803c568147f0dc790b5476b259d1e6fade
vincentmuriithi <vincentmuriithi06@gmail.com>
2023-08-11 12:04:38 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -5,12 +5,12 @@
 * Return: returns void
 */
 void *malloc_checked(unsigned int b)
-{ 
- void *ptr = malloc(b);
+{
+void *ptr = malloc(b);
 
- if (ptr == NULL)
-   {
-     exit(98);
-   }
- return (ptr);
+if (ptr == NULL)
+{
+exit(98);
+}
+return (ptr);
 }
