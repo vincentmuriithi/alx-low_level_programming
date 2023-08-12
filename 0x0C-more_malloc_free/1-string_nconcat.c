0276c5347b6d2c15e23c19d1152d89785e89c534
vincentmuriithi <vincentmuriithi06@gmail.com>
2023-08-12 02:57:17 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -21,11 +21,13 @@ i++;
 while (s2[k] != '\0')
 k++;
 
- if (n == NULL)
+ if (s2 == NULL)
    {
      s2 = "";
-     n = 0;
+     
    }
+ if (s1 == NULL)
+   s1 = "";
 ptr = (char *)malloc(sizeof(char) * (i + n + 1));
 
 if (ptr == NULL)
