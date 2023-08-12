a3081aea1930bc746b0ba4aa52ad767c395f2c44
vincentmuriithi <vincentmuriithi06@gmail.com>
2023-08-12 04:38:03 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -8,14 +8,8 @@
 */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-unsigned int i = 0, j = 0, k = 0, len1 = 0;
+unsigned int i, j = 0, k = 0, len1 = 0;
 char *ptr;
-
-while (*(s1 + len1) != '\0')
-len1++;
-
-while (s2[k] != '\0')
-k++;
 
 if (s2 == NULL)
 {
@@ -25,8 +19,16 @@ s2 = "";
 if (s1 == NULL)
 s1 = "";
 
+while (*(s1 + len1) != '\0')
+len1++;
+
+while (s2[k] != '\0')
+k++;
+
+
 if (n >= k)
 n = k;
+
 ptr = (char *)malloc(sizeof(char) * (len1 + n + 1));
 
 if (ptr == NULL)
