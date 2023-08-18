d6f5667f3f6e8b33372d9e4e8e21211779119bde
vincentmuriithi <vincentmuriithi06@gmail.com>
2023-08-18 02:11:24 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,16 @@
 #ifndef _DOG_
 #define  _DOG_
+/**
+ * struct dog - contains two char* datatypes and a float datapoint
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: the owner of the dog
+ */
 struct dog
 {
-  char *name;
-  float age;
-  char *owner;
+char *name;
+float age;
+char *owner;
 };
 #endif
 
