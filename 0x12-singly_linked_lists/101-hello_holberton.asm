9ad13fc4d06459f3f2e1bb19bb2de3432369aca0
vincentmuriithi <vincentmuriithi06@gmail.com>
2023-08-27 04:13:31 -0700

    101-hello_holberton.

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,44 +1,20 @@
-	.file	"ty.c"
-	.text
-	.section	.rodata
-.LC0:
-	.string	"Hello, Holberton"
-	.text
-	.globl	main
-	.type	main, @function
+section .data
+hello db "Hello, Holberton", 0
+newline db 10, 0   ; Newline character and null terminator
+
+section .text
+global main
+
+extern printf
+
 main:
-.LFB0:
-	.cfi_startproc
-	endbr64
-	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
-	movq	%rsp, %rbp
-	.cfi_def_cfa_register 6
-	leaq	.LC0(%rip), %rdi
-	call	puts@PLT
-	movl	$0, %eax
-	popq	%rbp
-	.cfi_def_cfa 7, 8
-	ret
-	.cfi_endproc
-.LFE0:
-	.size	main, .-main
-	.ident	"GCC: (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0"
-	.section	.note.GNU-stack,"",@progbits
-	.section	.note.gnu.property,"a"
-	.align 8
-	.long	 1f - 0f
-	.long	 4f - 1f
-	.long	 5
-0:
-	.string	 "GNU"
-1:
-	.align 8
-	.long	 0xc0000002
-	.long	 3f - 2f
-2:
-	.long	 0x3
-3:
-	.align 8
-4:
+push rbp
+mov rdi, hello
+call printf
+mov rdi, newline  ; Load the newline character
+call printf
+add rsp, 8
+mov rax, 0x60    ; Exit syscall
+xor rdi, rdi     ; Return code 0
+syscall
+
---
+++ 0x12-singly_linked_lists/ty.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main - entry of main program
+* Return: returns 0 upon success
+*/
+int main(void)
+{
+printf("Hello, Holberton\n");
+return (0);
+}
