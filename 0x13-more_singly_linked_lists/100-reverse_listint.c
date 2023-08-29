247c1942d1c33ae55bed8d5f0a286fee54d39224
vincentmuriithi <vincentmuriithi06@gmail.com>
2023-08-29 16:48:08 -0700

    100-reverse_listint.c

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,8 +1,7 @@
 #include "lists.h"
-
 /**
-* reverse_listint - reverses a linked list
-* @head: pointer to the first node in the list
+* reverse_listint - reverses a given linked list
+* @head: pointer to the first node in the linked list
 *
 * Return: pointer to the first node in the new list
 */
