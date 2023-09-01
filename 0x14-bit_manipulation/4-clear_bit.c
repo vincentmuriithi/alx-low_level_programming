9fb94f8658b8cb6d45d2906ee42b94518af9464f
vincentmuriithi <vincentmuriithi06@gmail.com>
2023-09-01 04:50:01 -0700

    4-clear_bit.c

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* clear_bit - sets the given bit to 0 at the given index
+* @n: input
+* @index: input value of index
+* Return: returns 1 upon success
+*/
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+unsigned long int mask;
+unsigned int bit_num;
+
+bit_num = sizeof(unsigned long int) * 8;
+
+if (bit_num >= index)
+return (-1);
+
+mask = 0UL << index;
+
+*n |= mask;
+return (1);
+}
