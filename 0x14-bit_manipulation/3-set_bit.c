5389d4b2e50050196deda8d00ef451a2c2e1d48e
vincentmuriithi <vincentmuriithi06@gmail.com>
2023-09-01 04:42:58 -0700

    3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -10,9 +10,9 @@ int set_bit(unsigned long int *n, unsigned int index)
 unsigned int numBits = sizeof(unsigned long int) * 8;
 unsigned long int mask;
 
-if (index >= numBits) {
+if (index >= numBits)
 return (-1);
-}
+
 
 mask = 1UL  << index;
 *n |= mask;
