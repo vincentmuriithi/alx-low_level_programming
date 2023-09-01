5a7c0a70084c4fb9bd45fe8588eeae2a5a1aa894
vincentmuriithi <vincentmuriithi06@gmail.com>
2023-09-01 02:30:10 -0700

    0-binary_to_uint.c

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+* binary_to_unit - converts binary to unsigned integer
+* @b: input pointer
+* Return: returns the unsigned integer
+*/
+unsigned int binary_to_uint(const char *b)
+{
+unsigned int i = 0, result = 0;
+char current;
+
+if (b == NULL)
+return (0);
+
+while (b[i] != '\0')
+{
+current = b[i];
+
+if (current == '0')
+result = result << 1;
+else if (current == '1')
+result = (result << 1) | 1;
+else
+return (0);
+
+i++;
+}
+
+return (result);
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,1 @@
+This directory contains bit manipulation project tasks
