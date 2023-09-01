1b7d2aaac2b8c970e73cd6ad94fee078a253cc9d
vincentmuriithi <vincentmuriithi06@gmail.com>
2023-09-01 04:37:42 -0700

    3-set_bit.c

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* set_bit - sets the bit to 1 at the given index
+* @n: input
+* @index: input index
+* Return: returns 1 upon success
+*/
+int set_bit(unsigned long int *n, unsigned int index)
+{
+unsigned int numBits = sizeof(unsigned long int) * 8;
+unsigned long int mask;
+
+if (index >= numBits) {
+return (-1);
+}
+
+mask = 1UL  << index;
+*n |= mask;
+
+return (1);
+}
