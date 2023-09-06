b918987f59f99a45e9c0f0ef0b9874cbd4929139
vincentmuriithi <vincentmuriithi06@gmail.com>
2023-09-06 14:53:53 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <fcntl.h>
 /**
 * read_textfile -
 * @filename: input
@@ -7,11 +8,11 @@
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-ssize_t fp, sz, read_bytes, written_bytes;
+ssize_t fp, read_bytes, written_bytes;
 char *buffer;
 
 if (filename == NULL)
-return (NULL);
+return (0);
 
 fp = open(filename, O_RDONLY);
 
@@ -28,6 +29,7 @@ return (0);
 read_bytes = read(fp, buffer, letters);
 if (read_bytes == -1)
 {
+free(buffer);
 close(fp);
 return (0);
 }
@@ -35,14 +37,18 @@ return (0);
 written_bytes = write(fp, buffer, read_bytes);
 if (written_bytes == -1)
 {
+free(buffer);
 close(fp);
 return (0);
 }
 
-if (written_bytyes != read_bytes || written_bytes < read_bytes)
+if (written_bytes != read_bytes || written_bytes < read_bytes)
 {
-cloes(fp);
+free(buffer);
+close(fp);
 return (0);
 }
+free(buffer);
+close(fp);
 return (written_bytes);
 }
