287c2f69c0e9aa9376838cab4506af5fed3f5eca
vincentmuriithi <vincentmuriithi06@gmail.com>
2023-10-14 19:57:20 -0700

    5-get_dnodeint.c

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+* get_dnodeint_at_index - gets the node at the given index
+* @head: input poineter
+* @index: the index  the node to be retrieved
+* Return: returns the retrieved node at given index
+*/
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+unsigned int i = 0;
+dlistint_t *tmp = head;
+
+if (!head)
+return (NULL);
+
+while (tmp && i < index)
+{
+tmp = tmp->next;
+i++
+}
+
+return (tmp);
+}
