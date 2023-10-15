12af795acf86305c13e6b9e36c58661b08f0553c
vincentmuriithi <vincentmuriithi06@gmail.com>
2023-10-14 19:21:51 -0700

    4-free_dlistint.c

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,20 +1,22 @@
 #include "lists.h"
 /**
- *
- */
+* free_dlistint - frees a list
+* @head: input pointer
+*/
 void free_dlistint(dlistint_t *head)
 {
-	dlistint_t *cr;
-	if (!head)
-		return;
-	while (head->prev)
-		head = head->prev;
+dlistint_t *cr;
 
-	while (head)
-	{
-		cr = head->next;
-		free(head);
-		head = cr;
-	}
+if (!head)
+return;
+while (head->prev)
+head = head->prev;
+
+while (head)
+{
+cr = head->next;
+free(head);
+head = cr;
+}
 
 }
