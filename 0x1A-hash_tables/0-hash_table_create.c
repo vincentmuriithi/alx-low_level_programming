b0a4c7e8265432fa1ffea6eba56f4d72b1cc3252
vincentmuriithi <vincentmuriithi06@gmail.com>
2023-11-05 20:12:00 -0800

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -16,7 +16,7 @@ hash_table = malloc(sizeof(hash_table_t));
 if (!hash_table)
 return (NULL);
 
-hash_table->array = malloc(sizeof(hash_node_t) * size);
+hash_table->array = malloc(sizeof(hash_node_t *) * size);
 if (!hash_table->array)
 {
 free(hash_table);
