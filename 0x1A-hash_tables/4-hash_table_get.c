9564050ccd4aa8a5745fec30c7e07c373b6af80b
vincentmuriithi <vincentmuriithi06@gmail.com>
2023-11-07 11:05:13 -0800

    4-hash_table_get.c

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,23 @@
+#include "hashtables.h"
+/**
+*
+*
+*
+*/
+
+char *hash_table_get(const hash_table_t *ht, const char *key) {
+unsigned long int index;
+hash_node_t *node;
+if (!ht || !key || strlen(key) == 0) {
+return NULL;
+}
+index = key_index((unsigned char *)key, ht->size);
+node = ht->array[index];
+while (node) {
+if (strcmp(node->key, key) == 0) {
+return node->value;
+}
+node = node->next;
+}
+return NULL;
+}
