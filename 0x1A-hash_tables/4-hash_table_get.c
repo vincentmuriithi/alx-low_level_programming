be1d4de0adcf7951606f236fb8b0b9550013451a
vincentmuriithi <vincentmuriithi06@gmail.com>
2023-11-07 18:17:12 -0800

    4-hash_table_get.c

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -22,17 +22,17 @@ node = node->next;
 return NULL;
 }
 /**
- * key_index - Retrieves the index of a key
- * @key: The key to be indexed
- * @size: The size of the hash table array
- *
- * Return: The computed index
- */
+* key_index - Retrieves the index of a key
+* @key: The key to be indexed
+* @size: The size of the hash table array
+*
+* Return: The computed index
+*/
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-    unsigned long int hash_value, index;
+unsigned long int hash_value, index;
 
-    hash_value = hash_djb2(key);
-    index = hash_value % size;
-    return (index);
+hash_value = hash_djb2(key);
+index = hash_value % size;
+return (index);
 }
