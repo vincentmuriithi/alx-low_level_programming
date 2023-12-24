4643e967179c045caac72d739f88ad4afd7c2db2
vincentmuriithi <vincentmuriithi06@gmail.com>
2023-12-24 01:04:19 -0800

    5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -4,6 +4,7 @@ module for calculating the perimeter
 of the given grid
 """
 
+
 def island_perimeter(grid):
     """
     function to calculate the perimeter of
@@ -19,9 +20,9 @@ def island_perimeter(grid):
     for i in range(len(grid)):
         for j in range(len(grid[0])):
             if grid[i][j] == 1:
-                perimeter +=4
+                perimeter += 4
 
-                if j > 0 and grid[i][j -1] == 1:
+                if j > 0 and grid[i][j - 1] == 1:
                     perimeter -= 2
 
                 if i > 0 and grid[i - 1][j]:
